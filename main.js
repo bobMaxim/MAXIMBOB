b47daf41ae62160591f248707f46fb3148f8d8ba
bobMaxim <161283665+bobMaxim@users.noreply.github.com>
2024-03-18 12:55:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+ let firstName = "Дмитрий";
+ let lastName = "Пекельник";
+ let fullName = firstName + " " + lastName;
+ console.log(fullName);
+
+ let a = 10;
+ let b = 3;
+ let c = 5;
+
+ console.log("a + b =", a + b);
+ console.log("a - b =", a - b);
+ console.log("a * b =", a * b);
+ console.log("a / b =", a / b);
+ console.log("a % b =", a % b);
+
+ let isTrue = true;
+ let isFalse = false;
+ console.log("isTrue && isFalse =", isTrue && isFalse);
+
+console.log("isTrue || isFalse =", isTrue || isFalse);
+ console.log("!isTrue =", !isTrue);
+
+ let counter = 10;
+ console.log("counter before increment:", counter);
+ counter++;
+ console.log("counter after increment:", counter);
+ counter--;
+ console.log("counter after decrement:", counter);
+
+ let greeting = "Привет";
+ let name = "Макс";
+ let message = greeting + ", " + name + "!";
+ console.log(message);
+
+ let prefix = "Это";
+ let middle = "тестовая";
+ let suffix = "строка";
+ let sentence = prefix + " " + middle + " " + suffix + ".";
+ let stn = `${prefix} ${middle} ${suffix}`
+ console.log(sentence);
+ console.log(stn);
+
+ let num1 = 15;
+ let num2 = 7;
+ let result = "Результат: " + (num1 + num2);
+ console.log(result);
+
+ let text = "Макс,Серега,Дима"; 
+ let upperCaseText = text.toUpperCase();
+let lowerCaseText = text.toLowerCase();
+console.log("Original Text:", text);
+console.log("Uppercase Text:", upperCaseText);
+console.log("Lowercase Text:", lowerCaseText);
+
+
